16eeda4097c61131ff8966f21d62509721c6acca
Bair <ber89ber89@gmail.com>
2024-12-19 15:00:50 +0800

    Not found page is added

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import classes from './Counter.module.scss';
 
 const Counter = () => {
@@ -6,7 +6,7 @@ const Counter = () => {
 
     const increment = () => {
         setCount(count + 1);
-    }
+    };
 
     return (
         <div className={classes.btn}>
---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,5 @@
+describe('Testing', () => {
+    test('test', () => {
+        expect(true).toBe(true)
+    })
+})
