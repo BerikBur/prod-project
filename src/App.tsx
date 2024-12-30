f99e255fdf416d228184dd53f1a088ac88ddeb80
Bair <ber89ber89@gmail.com>
2024-12-30 17:48:28 +0800

    linter errrors fixed

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,23 @@ import './styles/index.scss';
 import { AboutPage } from 'pages/AboutPage';
 import { MainPage } from 'pages/MainPage';
 import { classNames } from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
+import { RoutePath } from 'shared/config/routeConfig/routeConfig';
 import { useTheme } from './theme/useTheme';
 
 const App = () => {
     const { theme, toggleTheme } = useTheme();
+    const { t } = useTranslation();
 
     return (
         <div className={classNames('app', {}, [theme])}>
             <button onClick={toggleTheme}>TOGGLE</button>
-            <Link to="/">Главная</Link>
-            <Link to="/about">О сайте</Link>
+            <Link to={RoutePath.main}>{t('Главная')}</Link>
+            <Link to={RoutePath.about}>{t('О сайте')}</Link>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path="/about" element={<AboutPage />} />
-                    <Route path="/" element={<MainPage />} />
+                    <Route path={RoutePath.about} element={<AboutPage />} />
+                    <Route path={RoutePath.main} element={<MainPage />} />
                 </Routes>
             </Suspense>
         </div>
